5901a7092142b9c1b663e5f749525ac03fa7a943
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-05-29 19:25:50 -0300

    Corrigindo aplicação de atributos da classe exportados do json da API

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -18,6 +18,13 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,3));
+
+    }
+
     public String getNome() {
         return nome;
     }
@@ -76,6 +83,6 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public String toString() {
         return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento;
+                ", anoDeLancamento=" + anoDeLancamento + "," + "duração " + duracaoEmMinutos;
     }
 }
--- src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++ src/br/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -1,4 +1,4 @@
 package br.com.alura.screenmatch.modelos;
 
-public record TituloOmdb() {
+public record TituloOmdb(String title, String year, String runtime) {
 }
